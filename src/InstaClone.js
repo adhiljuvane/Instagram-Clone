ae78c534339537a27c87dd5009bd131d93bdacf6
Adhil Juvane <adhilmp3@gmail.com>
2019-06-24 11:41:47 +0530

    react-navigation

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from "react-native";
-import { PostFeed } from './components/container';
+import { View, StyleSheet } from "react-native";
+import { MainFeed, Login } from './components/screens';
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+
+const MainStack = createSwitchNavigator({
+    login: Login,
+    main: MainFeed
+});
+
+const AppContainer = createAppContainer(MainStack);
 
 class InstaClone extends Component {
     render() {
         return (
-            <View style={instaStyles.mainView}>
-                <View style={instaStyles.tempNavBar}>
-                    <Text>Instagram</Text>
-                </View>
-                <PostFeed />
-            </View>
+            <AppContainer />
         )
     }
 }
@@ -20,15 +23,6 @@ const instaStyles = StyleSheet.create({
         flex: 1,
         width: "100%",
         height: "100%"
-    },
-    tempNavBar: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: "100%",
-        height: 75,
-        backgroundColor: "rgb(250,250,250)",
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        borderBottomColor: "rgb(233,233,233)",
     },
 })
 
---
+++ src/components/screens/Login.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    onLogin = () => {
+        this.props.navigation.navigate("main")
+    }
+    render() {
+        return (
+            <TouchableOpacity style={loginStyles.mainView} onPress={() => { this.onLogin() }}>
+                <Text>Login Page</Text>
+            </TouchableOpacity>
+        )
+    }
+}
+
+const loginStyles = StyleSheet.create({
+    mainView: {
+        flex: 1,
+        height: "100%",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: 'center',
+    }
+})
+
+export default Login;
